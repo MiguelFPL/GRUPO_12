bb70aaab6dd2932a57c9e049c962c154840c19ab
laure <laure@DESKTOP-PO4OIDK>
2026-04-14 16:31:46 -0300

    Clase empleado

---
+++ TP2_GRUPO_12/src/ejercicio1/Empleado.java
@@ -0,0 +1,55 @@
+package ejercicio1;
+import java.time.LocalDate;
+
+public class Empleado extends Persona {
+	
+	private final int legajo;
+	private String puesto;
+	private static int cont = 999;
+	
+	//
+	
+	public static int devuelveProximoLegajo()
+	{
+		return cont+1;
+	}
+	
+	//
+	
+	public Empleado ()
+	{
+		super();
+		cont++;
+		this.legajo = cont;
+	}
+	
+	public Empleado (String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto)
+	{
+		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		cont++;
+		this.legajo = cont;
+		this.puesto = puesto;
+	}
+	
+	//
+	
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+	
+	//
+
+	@Override
+	public String toString() {
+		return "Empleado: [Legajo = " + legajo + ", Puesto = " + puesto +"]    " + super.toString();
+	}	
+}
+
