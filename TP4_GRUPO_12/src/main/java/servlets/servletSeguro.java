be5b08221a123142aa140a12c151f84a9c5fd0a9
laure <laure@DESKTOP-PO4OIDK>
2026-05-04 19:46:13 -0300

    servlet

--- TP4_GRUPO_12/src/main/java/servlets/servletSeguro.java
+++ TP4_GRUPO_12/src/main/java/servlets/servletSeguro.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.SeguroDao;
+import dominio.Seguro;
 
 /**
  * Servlet implementation class servletSeguro
@@ -26,8 +31,20 @@ public class servletSeguro extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int filas = 0;
+		
+		if (request.getParameter("btnAceptar") != null) {
+			Seguro s = new Seguro();
+			s.setDescripcion(request.getParameter("txtDescripcion"));
+			//s.setCostoContratacion(request.getParameter("txtCostoContratacion"));
+			//s.setCostoAsegurado(request.getParameter("txtCostoMaximo"));
+			
+			SeguroDao sd = new SeguroDao();
+			filas = sd.agregarSeguro(s);
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/AgregarSeguro.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
--- TP4_GRUPO_12/src/main/webapp/AgregarSeguro.jsp
+++ TP4_GRUPO_12/src/main/webapp/AgregarSeguro.jsp
@@ -13,13 +13,22 @@
 	<a href="AgregarSeguros.jsp">Agregar Seguros</a>
 	<br>
 	<a href="ListarSeguros.jsp">Listar Seguros</a>
-	<br> Agrergar Seguros Descripción
-	<input type="Text" name="txtDescripcion">
-	<br> Tipo de seguro
-	<br> Costo contratación
-	<input type="Text" name="txtCostoContratacion">
-	<br> Costo maximo asegurado
-	<input type="Text" name="txtCostoMaximo">
+	<br>
+	Agregar Seguros
+	<br>
+	IdSeguro
+	<br>
+	<form action="servletSeguro" method = "get">
+	Descripción <input type="Text" name="txtDescripcion">
+	<br>
+	Tipo de seguro
+	<br>
+	Costo contratación <input type="Text" name="txtCostoContratacion">
+	<br>
+	Costo maximo asegurado <input type="Text" name="txtCostoMaximo">
+	<br>
+	<input type = "submit" name = "bntAceptar" value = "Aceptar">
+	</form>
 
 </body>
 </html>
