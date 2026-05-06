2397c853a3430ea0b6e1b66895ae4db00e33cfcc
Martin_Forgaral <forgaralmartinnahuel@gmail.com>
2026-05-06 16:09:45 -0300

    Modifiqué el la query de agregarSeguro

--- TP4_GRUPO_12/src/main/java/dao/SeguroDao.java
+++ TP4_GRUPO_12/src/main/java/dao/SeguroDao.java
@@ -16,9 +16,9 @@ public class SeguroDao {
 	private String dbName = "segurosgroup";
 
 	public int agregarSeguro(Seguro seguro) {
-		String query = "Insert into seguros(idSeguro, descripcion, idTipo, costoContratacion, costoAsegurado) values ('"
-				+ seguro.getIdSeguro() + "','" + seguro.getDescripcion() + "','" + seguro.getTipoSeguros().getIdTipo()
-				+ "','" + seguro.getCostoContratacion() + "','" + seguro.getCostoAsegurado() + "')";
+		String query = "Insert into seguros(descripcion, idTipo, costoContratacion, costoAsegurado) VALUES ('"
+				+ seguro.getDescripcion() + "','" + seguro.getTipoSeguros().getIdTipo()
+				+ "','" + seguro.getCostoContratacion() + "','" + seguro.getCostoAsegurado();
 		Connection cn = null;
 		int filas = 0;
 
@@ -32,29 +32,12 @@ public class SeguroDao {
 		return filas;
 	}
 
-	public int obtenerProximoId() {
-		int proximoId = 1;
-		Connection cn = null;
-		try {
-			cn = DriverManager.getConnection(host + dbName, user, pass);
-			String query = "SELECT IFNULL(MAX(idSeguro), 0) + 1 as proximoId FROM seguros";
-			Statement st = cn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-			if (rs.next()) {
-				proximoId = rs.getInt("proximoId");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return proximoId;
-	}
-
 	public ArrayList<Seguro> obtenerTodosLosSeguros() {
 		ArrayList<Seguro> LSeguros = new ArrayList<Seguro>();
 		Connection cn = null;
 		try {
 			cn = DriverManager.getConnection(host + dbName, user, pass);
-			String query = "SELECT s.*, t.descripcion as tipoDescripcion FROM seguros s JOIN tiposeguros t ON s.idTipo = t.idTipo";
+			String query = "Select * from seguros";
 			Statement st = cn.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			while (rs.next()) {
@@ -66,7 +49,6 @@ public class SeguroDao {
 
 				TipoSeguros t = new TipoSeguros();
 				t.setIdTipo(rs.getInt("idTipo"));
-				t.setDescripcion(rs.getString("tipoDescripcion"));
 
 				x.setTipoSeguros(t);
 				LSeguros.add(x);
@@ -76,7 +58,7 @@ public class SeguroDao {
 		}
 		return LSeguros;
 	}
-
+	
 	public ArrayList<Seguro> obtenerSegurosPorTipo(int idTipo) {
 		ArrayList<Seguro> LSeguros = new ArrayList<Seguro>();
 		Connection cn = null;
@@ -104,4 +86,6 @@ public class SeguroDao {
 		}
 		return LSeguros;
 	}
+	
+	
 }
